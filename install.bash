64564f38246a4004cbd0bb7f772037dd9236b61f
Ben Finney <ben@benfinney.id.au>
2010-05-13 10:32:06 +1000

    Use safe quoting for filenames and variable expansions.

--- install.bash
+++ install.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # source the global config file
-source CONFIG
+source "./CONFIG"
 
 function svg_substitutions {
     # Process an SVG file with custom substitutions.
@@ -20,52 +20,52 @@ function svg_substitutions {
 
 # the basic cursors
 FILES="
-all-scroll 
-cell 
-col-resize 
-crosshair 
-default 
-e-resize 
-ew-resize 
-grabbing 
-n-resize 
-ne-resize 
-nesw-resize 
-not-allowed 
-ns-resize 
-nw-resize 
-nwse-resize 
-pencil 
-pirate 
-pointer 
-right-arrow 
-row-resize 
-s-resize 
-se-resize 
-sw-resize 
-text 
-up-arrow 
-vertical-text 
-w-resize 
+all-scroll
+cell
+col-resize
+crosshair
+default
+e-resize
+ew-resize
+grabbing
+n-resize
+ne-resize
+nesw-resize
+not-allowed
+ns-resize
+nw-resize
+nwse-resize
+pencil
+pirate
+pointer
+right-arrow
+row-resize
+s-resize
+se-resize
+sw-resize
+text
+up-arrow
+vertical-text
+w-resize
 X-cursor
 zoom-in
 zoom-out
 "
 
 
-if [ ! -d build ] ; then mkdir build; fi
-if [ ! -d tmp ] ; then mkdir tmp; fi
-if [ ! -d cursors ] ; then mkdir cursors; fi
-if [ ! -d shadows ] ; then mkdir shadows; fi
+if [ ! -d "build" ] ; then mkdir "build"; fi
+if [ ! -d "tmp" ] ; then mkdir "tmp"; fi
+if [ ! -d "cursors" ] ; then mkdir "cursors"; fi
+if [ ! -d "shadows" ] ; then mkdir "shadows"; fi
 
 
-for f in $FILES; do 
-  if [ -f svg/$f.svg ] ; then
-    svg_substitutions svg/$f.svg > tmp/tmp.svg
-    ./svg2png.bash $f
-  else
-    echo "skipping $f: no svg file found."
-  fi
+for f in $FILES; do
+    if [ -f "svg/$f.svg" ] ; then
+        svg_substitutions "svg/$f.svg" > "tmp/tmp.svg"
+        ./svg2png.bash "$f"
+    else
+        echo "skipping $f: no svg file found."
+    fi
 done
 
 # the pointer combined cursors
@@ -78,43 +78,43 @@ move
 no-drop
 "
 
-for f in $FILES; do 
-  if [ -f svg/$f.svg ] ; then
-    svg_substitutions svg/$f.svg > tmp/tmp.svg
-    ./svg2png.bash $f $f -BACKGROUND default -SHADOW move
-  else
-    echo "skipping $f: no svg file found."
-  fi
+for f in $FILES; do
+    if [ -f "svg/$f.svg" ] ; then
+        svg_substitutions "svg/$f.svg" > "tmp/tmp.svg"
+        ./svg2png.bash "$f" "$f" -BACKGROUND "default" -SHADOW "move"
+    else
+        echo "skipping $f: no svg file found."
+    fi
 done
 
 
 
-if [ ! -d build/help ] ; then
-	mkdir build/help
+if [ ! -d "build/help" ] ; then
+    mkdir "build/help"
 fi
-svg_substitutions svg/help1.svg > tmp/tmp.svg
-./svg2png.bash help -PART 1 -BACKGROUND default -SHADOW move -TIME 2000
-svg_substitutions svg/help2.svg > tmp/tmp.svg
-./svg2png.bash help -PART 2 -BACKGROUND default -SHADOW move -TIME 500
+svg_substitutions "svg/help1.svg" > "tmp/tmp.svg"
+./svg2png.bash "help" -PART 1 -BACKGROUND "default" -SHADOW "move" -TIME 2000
+svg_substitutions "svg/help2.svg" > "tmp/tmp.svg"
+./svg2png.bash "help" -PART 2 -BACKGROUND "default" -SHADOW "move" -TIME 500
 
 
-if [ ! -d build/progress ] ; then
-	mkdir build/progress
+if [ ! -d "build/progress" ] ; then
+    mkdir "build/progress"
 fi
-svg_substitutions svg/progress.svg > tmp/tmp.svg
+svg_substitutions "svg/progress.svg" > "tmp/tmp.svg"
 for (( i=1; $i < 25; i++ )); do
-  ./svg2png.bash progress -PART $i -BACKGROUND default -SHADOW move
-  patch -f --silent tmp/tmp.svg svg/progress.diff >> /dev/null
+    ./svg2png.bash "progress" -PART $i -BACKGROUND "default" -SHADOW "move"
+    patch -f --silent "tmp/tmp.svg" "svg/progress.diff" >> /dev/null
 done
 
 
-if [ ! -d build/wait ] ; then
-	mkdir build/wait
+if [ ! -d "build/wait" ] ; then
+    mkdir "build/wait"
 fi
-svg_substitutions svg/wait.svg > tmp/tmp.svg
+svg_substitutions "svg/wait.svg" > "tmp/tmp.svg"
 for (( i=1; $i < 37; i++ )); do
-  ./svg2png.bash wait -PART $i 
-  patch -f --silent tmp/tmp.svg svg/wait.diff >> /dev/null
+    ./svg2png.bash "wait" -PART $i
+    patch -f --silent "tmp/tmp.svg" "svg/wait.diff" >> /dev/null
 done
 
 # make and install
